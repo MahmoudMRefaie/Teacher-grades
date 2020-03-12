8924b8b9fb80e72c2a122b7a520daff66c75fc1f
MahmoudMRefaie <mahmoud.refaie22@gmail.com>
2020-03-12 23:02:05 +0200

    Using Fragment

--- app/src/main/java/com/example/teachergrades/StudentsGradesActivity.kt
+++ app/src/main/java/com/example/teachergrades/StudentsGradesActivity.kt
@@ -1,13 +1,46 @@
 package com.example.teachergrades
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_students_grades.*
 
-class StudentsGradesActivity : AppCompatActivity() {
+class StudentsGradesActivity : AppCompatActivity(), OnClickListener  {
+
+    var classAFrag = ClassAFragment()
+    var classBFrag = ClassBFragment()
+    var classCFrag = ClassCFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_students_grades)
+        classA.setOnClickListener(this)
+        classB.setOnClickListener(this)
+        classC.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.classA -> {
+                var transaction = supportFragmentManager.beginTransaction()
+                transaction.add(R.id.fragmentLayout , classAFrag , "CLASS A FRAGMENT")
+                transaction.commit()
+                hintToPutClass.text=""
+            }
+            R.id.classB -> {
+                var transaction = supportFragmentManager.beginTransaction()
+                transaction.add(R.id.fragmentLayout , classBFrag , "CLASS B FRAGMENT")
+                transaction.commit()
+                hintToPutClass.text=""
+            }
+            R.id.classC -> {
+                var transaction = supportFragmentManager.beginTransaction()
+                transaction.add(R.id.fragmentLayout , classCFrag , "CLASS C FRAGMENT")
+                transaction.commit()
+                hintToPutClass.text=""
+            }
+        }
     }
 }
