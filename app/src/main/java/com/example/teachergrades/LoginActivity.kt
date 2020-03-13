787124e7e7ee1b4fd19809ce32e6f03acccd1c83
MahmoudMRefaie <mahmoud.refaie22@gmail.com>
2020-03-13 02:22:21 +0200

    Using Fragment , SharedPreference

--- app/src/main/java/com/example/teachergrades/LoginActivity.kt
+++ app/src/main/java/com/example/teachergrades/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.teachergrades
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,12 +14,14 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-
+        val sharedPreferences = getSharedPreferences("LOGIN_INFO", Context.MODE_PRIVATE)
         login_btn.setOnClickListener({
             teacherName = enter_name.text.toString()
 
             var intent = Intent(applicationContext , StudentsGradesActivity::class.java)
+            intent.putExtra("Name",teacherName)
             startActivity(intent)
+            finish()
         })
 
     }
--- app/src/main/java/com/example/teachergrades/StudentsGradesActivity.kt
+++ app/src/main/java/com/example/teachergrades/StudentsGradesActivity.kt
@@ -5,23 +5,63 @@ import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Toast
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_students_grades.*
 
-class StudentsGradesActivity : AppCompatActivity(), OnClickListener  {
+class StudentsGradesActivity : AppCompatActivity()/*,OnClickListener*/ {
 
-    var classAFrag = ClassAFragment()
-    var classBFrag = ClassBFragment()
-    var classCFrag = ClassCFragment()
+    lateinit var classAFrag : ClassAFragment
+    lateinit var classBFrag : ClassBFragment
+    lateinit var classCFrag : ClassCFragment
+
+    var teacherName:String=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_students_grades)
-        classA.setOnClickListener(this)
-        classB.setOnClickListener(this)
-        classC.setOnClickListener(this)
+        //classA.setOnClickListener(this)
+        //classB.setOnClickListener(this)
+        //classC.setOnClickListener(this)
+
+        teacherName=getIntent().getStringExtra("Name")
+        hello.setText("Hello $teacherName")
+
+        bottomNavigation.setOnNavigationItemSelectedListener {item->
+            when(item.itemId)  {
+                R.id.classA -> {
+                    classAFrag = ClassAFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragmentLayout , classAFrag ,"CLASS A FRAGMENT")
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                    hintToPutClass.text=""
+                }
+                R.id.classB -> {
+                    classBFrag = ClassBFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragmentLayout , classBFrag ,"CLASS B FRAGMENT")
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                    hintToPutClass.text=""
+                }
+                R.id.classC -> {
+                    classCFrag = ClassCFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragmentLayout , classCFrag ,"CLASS C FRAGMENT")
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                    hintToPutClass.text=""
+                }
+            }
+            true
+        }
     }
 
-    override fun onClick(v: View) {
+    /*override fun onClick(v: View) {
         when(v.id){
             R.id.classA -> {
                 var transaction = supportFragmentManager.beginTransaction()
@@ -42,5 +82,6 @@ class StudentsGradesActivity : AppCompatActivity(), OnClickListener  {
                 hintToPutClass.text=""
             }
         }
-    }
+    }*/
+
 }
